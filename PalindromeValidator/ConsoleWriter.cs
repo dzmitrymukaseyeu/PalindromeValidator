37eb6df68a9352261b83088ed1232a5fed7d454a
Nikita Starostin <57386894+nikita-starostin@users.noreply.github.com>
2024-01-21 13:36:03 +0000

    get rid of classes

--- PalindromeValidator/ConsoleWriter.cs
+++
@@ -1,9 +0,0 @@
-namespace PalindromeValidator;
-
-public class ConsoleWriter
-{
-    public void Print(string result)
-    {
-        Console.WriteLine(result);
-    }
-}
--- PalindromeValidator/Program.cs
+++ PalindromeValidator/Program.cs
@@ -1,12 +1,21 @@
 using PalindromeValidator;
 
-var reader = new ConsoleReader();
 var checker = new PalindromChecker();
-var printer = new ConsoleWriter();
 
-var inputValue = reader.Read();
+var inputValue = Read();
 var (isPalindrome, error) = checker.IsPalindrome(inputValue);
-printer.Print(error ?? GetSuccessMessage(isPalindrome, inputValue));
+Write(error ?? GetSuccessMessage(isPalindrome, inputValue));
 
 string GetSuccessMessage(bool isPalindrome, string value) =>
     isPalindrome ? $"{value} is palindrome" : $"{value} is not palindrome";
+
+string Read()
+{
+    Console.WriteLine("Enter value:");
+    return Console.ReadLine();
+}
+
+void Write(string result)
+{
+    Console.WriteLine(result);
+}
