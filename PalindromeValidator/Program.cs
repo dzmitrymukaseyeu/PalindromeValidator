87e366a6fa881aea1c8f732170676ce8813d3178
Nikita Starostin <57386894+nikita-starostin@users.noreply.github.com>
2024-01-21 13:29:47 +0000

    Get rid of infinite loop

--- PalindromeValidator/Program.cs
+++ PalindromeValidator/Program.cs
@@ -4,11 +4,8 @@ var reader = new ConsoleReader();
 var checker = new PalindromChecker();
 var printer = new ConsoleWriter();
 
-while (true)
-{
-    var inputValue = reader.Read();
-    var (isPalindrome, errorMessage) = checker.IsPalindrome(inputValue);
-    var result = errorMessage ?? (isPalindrome ? $"{inputValue} is palindrome" : $"{inputValue} is not palindrome");
-    
-    printer.Print(result); 
-}
+var inputValue = reader.Read();
+var (isPalindrome, errorMessage) = checker.IsPalindrome(inputValue);
+var result = errorMessage ?? (isPalindrome ? $"{inputValue} is palindrome" : $"{inputValue} is not palindrome");
+
+printer.Print(result);
